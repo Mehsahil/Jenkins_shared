15cfaaf73b65d883971160e08bbc355937752f29
Dhiraj <work.dhiraj22@gmail.com>
2026-04-06 22:51:58 +0530

    Create trivy_imagescan.groovy

---
+++ vars/trivy_imagescan.groovy
@@ -0,0 +1,7 @@
+def call (String ImageName, String ImageTag) {
+    withCredentials (usernamePassword[
+        credentialsId: "Docker", 
+        usernameVariable : "DockerUser"]) {
+            sh "trivy image ${DockerUser}/${ImageName}:${ImageTag} --severity CRITICAL,HIGH"
+        }
+}
