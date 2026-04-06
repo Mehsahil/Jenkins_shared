6e2cb73be7d636704091d46c8d4065da1ffdd7ee
Dhiraj <work.dhiraj22@gmail.com>
2026-04-06 23:22:18 +0530

    Update trivy_imagescan.groovy

--- vars/trivy_imagescan.groovy
+++ vars/trivy_imagescan.groovy
@@ -2,7 +2,7 @@ def call (String ImageName, String ImageTag, String Directory) {
     withCredentials([usernamePassword(
         credentialsId:"Docker",
         passwordVariable: "DockerPass",
-        usernameVariable: "DockerUser")]) {
-            sh "trivy image ${DockerUser}/${ImageName}:${ImageTag} > trivy-${Directory}-image.txt"
+        usernameVariable : "DockerUser")]) {
+            sh "trivy image ${DockerUser}/${ImageName}:${ImageTag} > trivy-${Directory}-image.txt 2>&1"
         }
 }
