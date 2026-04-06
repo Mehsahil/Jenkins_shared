0db75515494bdfd63401ce643d9376d2db813ca3
Dhiraj <work.dhiraj22@gmail.com>
2026-04-06 22:41:53 +0530

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,13 @@
+def call (String ImageName, String ImageTag, String Directory) {
+    withCredentials (usernamePassword[
+        credentialsId:"Docker",
+        passwordVariable: "DockerPass",
+        usernameVariable: "DockerUser"]) {
+            echo "Checking Inn Docker Login.. "
+            sh " docker login -u ${DockerUser} -p ${DockerPass}"
+
+            echo " Build image is now being pushed to DockerHub"
+            sh "docker push ${DockerUser}/${ImageName}:${ImageTag}"
+        }
+        echo "Docker image has been pushed for Directory '${Directory}'"
+}
